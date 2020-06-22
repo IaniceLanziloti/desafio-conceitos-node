74e651a62652a7e8b1d53796efa0f448ae57c2ea
Ianice Lanziloti <ianice_lanziloti@hotmail.com>
2020-06-22 14:44:02 -0300

    Retornando mensagem de erro ao não encontrar o repositório

--- src/app.js
+++ src/app.js
@@ -21,7 +21,13 @@ const validateUuid = (request, response, next) => {
 const findRepository = (request, response, next) => {
   const { id } = request.params;
 
-  const repositoryIndex = repositories.findIndex((repository) => repository.id === id);
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found.' });
+  }
 
   response.locals.repositoryIndex = repositoryIndex;
 
